44c459a106cfb93a46b6ac3afa5f5aecb99c73da
rafaDev <rafa.rodriguez@skydropx.com>
2024-07-15 19:49:21 -0400

    feat: add constants

---
+++ config/initializers/constants.rb
@@ -0,0 +1,9 @@
+module Constants
+
+  EMAIL_REGEX = /\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}\b/i
+  EMAIL_ERROR = 'Invalid email format'
+  NAME_ERROR = 'Missing name'
+  AGE_ERROR = 'Too young'
+  USER_INVALID = 'User invalid'
+
+end
