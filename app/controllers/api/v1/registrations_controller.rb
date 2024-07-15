23ff6046a71dc7f0e16c9203a8398109340dfc39
rafaDev <rafa.rodriguez@skydropx.com>
2024-07-15 19:51:00 -0400

    refactor: add interactor CreteUser

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,32 +3,11 @@ class Api::V1::RegistrationsController < Devise::RegistrationsController
 
   # Sign up
   def create
-    user = User.new(user_params)
-
-    
-    # byebug
-    # https://medium.com/@lucaspolonuer/ruby-on-rails-boost-your-software-design-skills-with-interactors-e07f1f9a711e
-    
-    return render_name_error if user.name.nil?
-    return render_age_error if Time.now.year - user.borned_at.year < 18
-
-    if user.save
-
-      UserMailer.welcome_mail(user).deliver_now
-
-      json_response "Signed Up Succesfully", true, { user: user }, :ok
-    else
-      json_response "Something wrong", false, {}, :unprocessable_entity
-    end
+    result = CreateUser.call(user_params)
+    return render status: :unprocessable_entity, json: result.error unless result.success?
+    json_response "Signed Up Succesfully", true, { user: result.user }, :ok
   end
 
-  def render_name_error
-    json_response "Missing name", false, {}, :unprocessable_entity
-  end
-
-  def render_age_error
-    json_response "Too young", false, {}, :unprocessable_entity
-  end
   private
 
   def user_params
