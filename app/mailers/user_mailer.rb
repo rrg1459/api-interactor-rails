d3ed0694c1b6f78c6bf912e37cd5ad6b0cc9ee1d
rafaDev <rafa.rodriguez@skydropx.com>
2024-07-16 08:54:11 -0400

    feat: create method campaign_mail

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,10 +1,14 @@
 class UserMailer < ApplicationMailer
   default from: 'DRIWLER <info@driwler.com>'
 
-	# -----------------------------------------------------------------------------------------
   def welcome_mail(user)
     @user = user
     mail(to: @user.email, subject: 'Bienvenido a los juegos del hambre')
+  end
+
+  def campaign_mail(user)
+    @user = user
+    mail(to: @user.email, subject: 'Campaña de los juegos del hambre')
   end
 
 end
